1e8efbb0599f1ca0e9881053a31268d3d8a366e7
Tem <54119870+Teampris@users.noreply.github.com>
2023-04-20 01:18:32 +0200

    Add basic styles and black pixel paint

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const container = document.getElementById('container');
 const randomColor = () => Math.floor(Math.random() * 256);
 for(let i = 1 ; i <= 16 ; i++) {
     for(let j = 1 ; j <= 16 ; j++) {
-        pixel = document.createElement('div');
+        const pixel = document.createElement('div');
         pixel.setAttribute('id', `${i};${j}`);
         pixel.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
         pixel.style.width = '16px';
         pixel.style.height = '16px';
         container.appendChild(pixel);
         console.log(pixel.id);
-
+        pixel.addEventListener('mouseover', function() {
+            this.style.backgroundColor = 'black';
+        });
     }
 }
