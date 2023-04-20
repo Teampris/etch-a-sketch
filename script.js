67c324ec26390ff9cb2e13e58e9c26c7c23fbeff
Tem <54119870+Teampris@users.noreply.github.com>
2023-04-21 01:52:30 +0200

    Add three buttons with color functions

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const container = document.getElementById('container');
 const randomColor = () => Math.floor(Math.random() * 256);
+const blackButton = document.getElementById('btn-1')
+const eraseButton = document.getElementById('btn-2')
+const rainbowButton = document.getElementById('btn-3')
+
+let currentFunction = 'black';
+
+function setFunction(event) {
+    currentFunction = event.target.dataset.function;
+  }
+
 for(let i = 1 ; i <= 16 ; i++) {
     for(let j = 1 ; j <= 16 ; j++) {
         const pixel = document.createElement('div');
@@ -14,3 +24,20 @@ for(let i = 1 ; i <= 16 ; i++) {
         });
     }
 }
+
+function handleHover(event) {
+    if (currentFunction === 'black') {
+      event.target.style.backgroundColor = 'black';
+    } else if (currentFunction === 'erase') {
+      event.target.style.backgroundColor = 'white';
+    } else if (currentFunction === 'rainbow') {
+      const randomColor = () => Math.floor(Math.random() * 256);
+      event.target.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+    }
+  }
+
+blackButton.addEventListener('click', setFunction);
+eraseButton.addEventListener('click', setFunction);
+rainbowButton.addEventListener('click', setFunction);
+
+container.addEventListener('mouseover', handleHover);
