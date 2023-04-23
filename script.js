7d1ba088ade9db8296b88e44de893541d39f7930
Tem <54119870+Teampris@users.noreply.github.com>
2023-04-23 22:39:12 +0200

    Add canvas size change

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.getElementById('container');
 const randomColor = () => Math.floor(Math.random() * 256);
-const blackButton = document.getElementById('btn-1')
-const eraseButton = document.getElementById('btn-2')
-const rainbowButton = document.getElementById('btn-3')
+const blackButton = document.getElementById('btn-1');
+const eraseButton = document.getElementById('btn-2');
+const rainbowButton = document.getElementById('btn-3');
+const sizeButton = document.getElementById('size');
+
+let canvasSize = 16;
 
 let currentFunction = 'black';
 
@@ -10,19 +13,29 @@ function setFunction(event) {
     currentFunction = event.target.dataset.function;
   }
 
-for(let i = 1 ; i <= 16 ; i++) {
-    for(let j = 1 ; j <= 16 ; j++) {
-        const pixel = document.createElement('div');
-        pixel.setAttribute('id', `${i};${j}`);
-        pixel.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
-        pixel.style.width = '16px';
-        pixel.style.height = '16px';
-        container.appendChild(pixel);
-        console.log(pixel.id);
-        pixel.addEventListener('mouseover', function() {
-            this.style.backgroundColor = 'black';
-        });
+
+function createCanvas() {
+  for(let i = 1 ; i <= canvasSize ; i++) {
+    for(let j = 1 ; j <= canvasSize ; j++) {
+      const pixel = document.createElement('div');
+      pixel.setAttribute('id', `${i};${j}`);
+      pixel.style.backgroundColor = 'white';
+      pixel.style.width = `${256/canvasSize}px`;
+      pixel.style.height = `${256/canvasSize}px`;
+      container.appendChild(pixel);
+      console.log(pixel.id);
+      pixel.addEventListener('mouseover', function() {
+          this.style.backgroundColor = 'black';
+      });
     }
+  }
+}
+
+function deleteCanvas() {
+  const canvasPixels = document.querySelectorAll('#container div');
+  for (let i = 0; i < canvasPixels.length; i++) {
+    canvasPixels[i].remove();
+  }
 }
 
 function handleHover(event) {
@@ -36,8 +49,18 @@ function handleHover(event) {
     }
   }
 
+function setSize(event) {
+  canvasSize = prompt('set size of canvas up to 100 pixels');
+  deleteCanvas();
+  createCanvas();
+}
+
 blackButton.addEventListener('click', setFunction);
 eraseButton.addEventListener('click', setFunction);
 rainbowButton.addEventListener('click', setFunction);
 
 container.addEventListener('mouseover', handleHover);
+
+sizeButton.addEventListener('click', setSize);
+
+createCanvas();
